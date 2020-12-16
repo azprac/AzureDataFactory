71650dceb22acab03e400bacd844ea44a7cb67c2
azprac <76025203+azprac@users.noreply.github.com>
2020-12-16 11:09:36 -0500

    Create Customer.sql

---
+++ Customer.sql
@@ -0,0 +1,14 @@
+create table customer(
+customer_dim_key decimal(25),
+first_name varchar(250),
+last_name varchar(250),
+middle_name varchar(250),
+address varchar(2500),
+city  varchar(250),
+state varchar(250),
+zip_code varchar(20),
+customer_number BIGINT,
+eff_start_date date,
+eff_end_date date,
+is_active varchar(5)
+)
